f840c525cc4ca7aaf74a794d77992312986225b3
Rajakumardev <rajakumarofficial@gmail.com>
2022-12-22 20:39:16 +0530

    globals.css added and body classes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
+import "../styles/globals.css";
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <body className="bg-primary-900 w-screen h-screen">{children}</body>
     </html>
-  )
+  );
 }
