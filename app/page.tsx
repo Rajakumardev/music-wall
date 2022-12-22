392a9ab80927c6136e9ce20b993d8c86ffc1bf63
Rajakumardev <rajakumarofficial@gmail.com>
2022-12-22 20:42:04 +0530

    home page basic

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,15 @@
+import { PuffLoader } from "react-spinners";
+
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <div className="flex justify-center items-center w-screen h-screen">
+      <div className="bg-accent-900 text-primary-900:w p-4 rounded-l-full rounded-r-full flex justify-start items-center gap-4 w-80">
+        <div className="">
+          <PuffLoader color="#fff" size="20" loading={true}/>
+        </div>
+        <div className="text-primary-900 font-semibold"> currently playing </div>
+      </div>
+    </div>
+  );
 };
 export default Home;
