38a93808dffcc5baaa046c4a3cf11bf57c1240ba
Rajakumardev <rajakumarofficial@gmail.com>
2022-12-23 08:13:04 +0530

    added auth endpoints

--- constants/api.constants.ts
+++ constants/api.constants.ts
@@ -1,2 +1,4 @@
 export const SPOTIFY_API_ROOT: string = "https://api.spotify.com/v1/";
-export const SPOTIFY_PLAYING_NOW_ENDPOINT = `${SPOTIFY_API_ROOT}me/player/currently-playing`;
+export const SPOTIFY_AUTH_API_ROOT: string = "https://accounts.spotify.com/";
+export const SPOTIFY_PLAYING_NOW_ENDPOINT:string = `${SPOTIFY_API_ROOT}me/player/currently-playing`;
+export const SPOTIFY_TOKEN_ENDPOINT:string = `${SPOTIFY_AUTH_API_ROOT}api/token`;
