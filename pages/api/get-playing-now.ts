6892673210d981c88d99fa3795ddae5ebc9ac53c
Rajakumardev <rajakumarofficial@gmail.com>
2022-12-23 08:13:44 +0530

    basic get current song

--- pages/api/get-playing-now.ts
+++ pages/api/get-playing-now.ts
@@ -1,11 +1,15 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getPlayingNow } from "../../utils/spotify.utils";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const response = getPlayingNow("asdfasdf");
-  const {item = {}} = response;
-  const {name = ""} = item;
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const response = await getPlayingNow("asdfasdf");
+  //const { item = {} } = response;
+  //const { name = "" } = item;
   res.status(200).json({
-    songName: name
+    //songName: name,
+    response
   });
 }
--- utils/spotify.utils.ts
+++ utils/spotify.utils.ts
@@ -1,10 +1,18 @@
 import axios from "axios";
-import { SPOTIFY_PLAYING_NOW_ENDPOINT } from "../constants";
+import {
+  SPOTIFY_PLAYING_NOW_ENDPOINT,
+  SPOTIFY_REFRESH_TOKEN_KEY,
+} from "../constants";
+import { getEnvVariable } from "./env.utis";
 
 export const getPlayingNow = async (clientId: string) => {
   try {
     const endpoint = `${SPOTIFY_PLAYING_NOW_ENDPOINT}`;
-    const response = await axios.get(endpoint);
+    const response = await axios.get(endpoint, {
+      headers: {
+        Authorization: `BEARER ${getEnvVariable(SPOTIFY_REFRESH_TOKEN_KEY)}`,
+      },
+    });
     console.log("/get-playing-now:", response);
     return response;
   } catch (error) {
