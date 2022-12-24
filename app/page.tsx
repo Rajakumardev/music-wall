02a75b4f6404a85ae82ca10ba47e353e32a739bc
Rajakumardev <rajakumarofficial@gmail.com>
2022-12-24 17:46:21 +0530

    tag like artist list

--- app/page.tsx
+++ app/page.tsx
@@ -7,27 +7,54 @@ const Home = async () => {
   console.log(GET_PLAYING_NOW_EP);
   const response = await axios.get(`${GET_PLAYING_NOW_EP}`);
   const { data = {} } = response;
-  const { player = "offline", name = "", trackUrl = "#" } = data;
+  const { player = "offline", name = "", trackUrl = "#", artists = [] } = data;
   const currentlyPlayingText = name ? name : player;
   const iconName = name ? "music" : "mute";
+  console.log("artists:", artists);
   return (
     <div className="flex justify-center items-center w-screen h-screen">
-      <Link
-        href={trackUrl}
-        target="_blank"
-        title="Open the song in new tab."
-        className="bg-accent-900 text-primary-900:w p-4 rounded-l-full rounded-r-full flex justify-start items-center gap-4 w-80"
-      >
-        <div className="">
-          <IconFactory
-            icon={iconName}
-            className="w-5 h-5 animate-pulse fill-primary-900"
-          />
-        </div>
-        <div className={`text-primary-900 font-semibold truncate`}>
-          {currentlyPlayingText}
-        </div>
-      </Link>
+      <div>
+        <Link
+          href={trackUrl}
+          target="_blank"
+          title="Open the song in new tab."
+          className="bg-accent-900 text-primary-900 p-4 rounded-l-full rounded-r-full flex justify-start items-center gap-4 w-80"
+        >
+          <div className="">
+            <IconFactory
+              icon={iconName}
+              className="w-5 h-5 animate-pulse fill-primary-900"
+            />
+          </div>
+          <div className={`text-primary-900 font-semibold truncate`}>
+            {currentlyPlayingText}
+          </div>
+        </Link>
+        {artists.length > 0 && (
+          <div className="mt-4 w-80 border-accent-900 border-2 p-4 rounded-lg">
+            <h3 className="text-accent-900">Artist[s]</h3>
+            <div className="flex w-80 text-accent-900 mt-4 rounded-l-sm flex-wrap gap-4">
+              {artists.map((artist: any) => {
+                const {
+                  name: artistName = "",
+                  external_urls: externalArtistUrl = {},
+                } = artist;
+                const { spotify: spotifyArtistUrl = "#" } = externalArtistUrl;
+
+                return (
+                  <Link
+                    href={spotifyArtistUrl}
+                    target="_blank"
+                    className="text-xs bg-accent-900 text-primary-900 p-2 font-semibold rounded-l-full rounded-r-full"
+                  >
+                    {artistName}
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
