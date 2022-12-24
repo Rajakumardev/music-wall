fc630de127e0f8f7a381cf125f1d7fde94c699a6
Rajakumardev <rajakumarofficial@gmail.com>
2022-12-24 11:50:10 +0530

    Moved the condition check out of the jsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ const Home = async () => {
   const { data = {} } = response;
   const { is_playing = "", player = "offline" } = data;
   const { name = "" } = data;
-
+  const currentlyPlayingText = name ? name : player;
   return (
     <div className="flex justify-center items-center w-screen h-screen">
       <div className="bg-accent-900 text-primary-900:w p-4 rounded-l-full rounded-r-full flex justify-start items-center gap-4 w-80">
@@ -16,7 +16,7 @@ const Home = async () => {
           <PuffLoader color="#fff" size="20" loading={true} />
         </div>
         <div className={`text-primary-900 font-semibold`}>
-          {player === "offline" ? `${player} ` : name}
+          {currentlyPlayingText}
         </div>
       </div>
     </div>
