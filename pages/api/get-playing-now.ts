44e09d45281b011a7fa5d0e51bb435a43c797a74
Rajakumardev <rajakumarofficial@gmail.com>
2022-12-24 17:10:32 +0530

    handle item null

--- pages/api/get-playing-now.ts
+++ pages/api/get-playing-now.ts
@@ -26,6 +26,15 @@ export default async function handler(
   }
 
   const { item = {} } = response;
+
+  //handle the null item -> idk why this happening may be i am getting ratelimited.
+  if (item === null) {
+    res.status(200).json({
+      player: "Not listening.",
+      is_playing: false,
+    });
+  }
+
   const {
     album = {},
     artists = {},
