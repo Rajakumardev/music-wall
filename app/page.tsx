972496b4421b91868a72fe4a35c0d1738f39a83d
Rajakumardev <rajakumarofficial@gmail.com>
2022-12-24 12:12:59 +0530

    icon added with basix tw animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import { PuffLoader } from "react-spinners";
 import { GET_PLAYING_NOW_EP } from "../constants";
 import axios from "axios";
+import { MusicSign } from "../components";
 
 const Home = async () => {
   console.log(GET_PLAYING_NOW_EP);
@@ -13,7 +13,7 @@ const Home = async () => {
     <div className="flex justify-center items-center w-screen h-screen">
       <div className="bg-accent-900 text-primary-900:w p-4 rounded-l-full rounded-r-full flex justify-start items-center gap-4 w-80">
         <div className="">
-          <PuffLoader color="#fff" size="20" loading={true} />
+          <MusicSign className="w-5 h-5 animate-pulse fill-primary-900" />
         </div>
         <div className={`text-primary-900 font-semibold`}>
           {currentlyPlayingText}
